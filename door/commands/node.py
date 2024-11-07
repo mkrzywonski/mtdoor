25b962321f4d0ec3d13c0858bd324ddbf48000f2
Mike Krzywonski <github@krzywonski.me>
2024-11-07 23:25:53 +0000

    node sn for node list by short name, and node <shortName> for node detail

--- door/commands/node.py
+++ door/commands/node.py
@@ -1,17 +1,19 @@
 from loguru import logger as log
-
 from . import BaseCommand
 from ..models import NodeInfo
 
 
-def format_node_list(nodes: list[NodeInfo]) -> str:
+def format_node_list(nodes: list[NodeInfo], msg) -> str:
     """
     output suitable for a list of nodes
     """
     response = ""
     n: NodeInfo
     for n in nodes:
-        r = f"{n.user.id}: {n.user.longName} ({n.user.shortName})\n"
+        if msg == "sn":
+            r = f"{n.user.shortName}\n"
+        else:
+            r = f"{n.user.id}\n"
 
         if len(response) + len(r) > 200:
             break
@@ -26,7 +28,8 @@ def format_node_detail(n: NodeInfo) -> str:
     output for detail of a node
     """
 
-    reply = f"""Name: {n.user.longName} ({n.user.shortName})
+    reply = f"""ID: {n.user.id}
+Name: {n.user.longName} ({n.user.shortName})
 Last heard {n.last_heard.strftime("%Y-%m-%d %H:%M")}
 SNR {n.snr}, Hops {n.hopsAway}"""
 
@@ -41,13 +44,13 @@ Batt.: {n.deviceMetrics.batteryLevel}% {n.deviceMetrics.voltage:.3}V
 Util.: {n.deviceMetrics.channelUtilization} ch, {n.deviceMetrics.airUtilTx:.2} air
 Up: {n.deviceMetrics.uptimeSeconds}s"""
 
-    return reply
+    return reply[:200]
 
 
 class NodeQuery(BaseCommand):
     command = "node"
     description = "read from my device node DB"
-    help = "'node' for list\n'node <!id>' for detail\n'node me' for yours\n'node you' for mine"
+    help = "'node' or 'node id' for NodeID list\n'node sn' for short name list\n'node <!id>' or 'node <name> for detail\n'node me' for yours\n'node you' for mine"
 
     node_list_count: int = 5
 
@@ -56,7 +59,7 @@ class NodeQuery(BaseCommand):
         msg = msg[len(self.command) :].lstrip()
 
         # they want a list
-        if msg == "":
+        if msg == "" or msg == "id" or msg == "sn":
             n: NodeInfo
             ns: list[NodeInfo] = []
 
@@ -66,7 +69,7 @@ class NodeQuery(BaseCommand):
                 if n.user.id == self.interface.getMyUser()["id"]:
                     continue
                 ns.append(n)
-            return format_node_list(ns)
+            return format_node_list(ns, msg)
 
         # they want to know about themselves
         elif msg.strip().lower() == "me":
@@ -78,7 +81,16 @@ class NodeQuery(BaseCommand):
             n = NodeInfo(**self.interface.getMyNodeInfo())
             return format_node_detail(n)
 
-        # they want information about someone else
+        # they want to know about a short name
+        elif msg.strip().lower()[:1] != "!":
+            n: NodeInfo
+            for n in self.interface.nodes.values():
+                n = NodeInfo(**n)
+                if n.user.shortName.strip().lower() == msg.strip().lower():
+                    return format_node_detail(n)
+            return "I can't find that node. Use the short name or the hex identifier that begins with '!'."
+
+        # they want information about a node ID
         else:
             try:
                 n: NodeInfo = self.get_node(msg)
@@ -88,4 +100,4 @@ class NodeQuery(BaseCommand):
             if n:
                 return format_node_detail(n)
             else:
-                return "I can't find that node. Use the hex identifier that begins with '!'."
+                return "I can't find that node. Use the short name or the hex identifier that begins with '!'."
